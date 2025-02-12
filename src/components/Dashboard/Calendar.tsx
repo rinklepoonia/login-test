1556b5b23ed52d61e061acad401b1d7d4dc40b64
rinklepoonia <rinklepoonia889@gmail.com>
2025-02-12 12:48:50 +0530

    remove unused code

--- src/components/Dashboard/Calendar.tsx
+++ src/components/Dashboard/Calendar.tsx
@@ -5,18 +5,9 @@ const Calendar = () => {
 
   return (
     <div className="flex flex-col items-center justify-center py-10 px-4 container">
-      {/* {rootElement && ( 
-        <PopupWidget
-          url="https://calendly.com/rinklepoonia889/30min?back=1&month=2025-02"
-          rootElement={rootElement}
-          text="Click here to schedule!"
-          textColor="#ffffff"
-          color="#00a2ff"
-        />
-      )} */}
-        <InlineWidget 
+      <InlineWidget 
         url="https://calendly.com/rinklepoonia889/30min?back=1&month=2025-02" 
-        styles={{ width: "100%", height: "700px" }} // Adjust height as needed
+        styles={{ width: "100%", height: "700px" }}
       />
     </div>
   );
